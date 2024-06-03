af225129a9cbfe8fa5a565c157d9b5103e706ede
Adarsh <adarsh9540984202@gmail.com>
2024-06-04 00:12:02 +0530

    update the button

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,27 @@
-import './App.css'
-import Navbar from './components/Navbar'
-import Card from './components/Card'
+
+import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
+import './App.css';
+import Navbar from './components/Navbar';
+import Card from './components/Card';
 import { KindeProvider } from "@kinde-oss/kinde-auth-react";
+
 
 function App() {
   return (
     <KindeProvider
-    clientId="628c70d7f2bb4ac3a7c0cc99e9cc2dc5"
-    domain="https://xplorify.kinde.com"
-    redirectUri="http://localhost:3000"
-    logoutUri="http://localhost:3000"
+      clientId="628c70d7f2bb4ac3a7c0cc99e9cc2dc5"
+      domain="https://xplorify.kinde.com"
+      redirectUri="http://localhost:5173"
+      logoutUri="http://localhost:5173"
     >
-      <>
-
-        <Navbar />
-        <Card />
-      </>
+      <Router>
+        <Routes>
+          <Route path="/" element={<><Navbar /><Card /></>} />
+          <Route path="*" element={<Navigate to="/" />} />
+        </Routes>
+      </Router>
     </KindeProvider>
   );
 }
 
-export default App
+export default App;
